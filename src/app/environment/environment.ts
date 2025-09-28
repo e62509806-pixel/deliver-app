addac4c86242494cc2561ce69875cf9a5eefd2e7
Fernan <e62509806@gmail.com>
2025-09-28 12:04:45 -0300

    feat: Add Bootstrap 5.3.3 integration with styles and example components

---
+++ src/app/environment/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://dzeawanrkskzsorkyrxi.supabase.co/rest/v1',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6ZWF3YW5ya3NrenNvcmt5cnhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNjcyNTEsImV4cCI6MjA3NDY0MzI1MX0.6v0eK0oCpXztlmg7m1ENZ3FonG1bRx9MGHNQLrO39Hw',
+};
